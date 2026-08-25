6df0543866156cc318377316b7d5780883cabad6
Jeancarlo Daniel Rodríguez Pivaral <149130487+daniel102sa1@users.noreply.github.com>
2026-08-24 22:09:51 -0600

    feat: add locked plans page

---
+++ app/plans/page.tsx
@@ -0,0 +1,7 @@
+import LockedPlansDashboard from "@/components/LockedPlansDashboard";
+
+export const dynamic = "force-dynamic";
+
+export default function PlansPage() {
+  return <LockedPlansDashboard />;
+}
