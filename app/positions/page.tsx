5bae27d9712c8e32478c3662704832458ebaded5
Jeancarlo Daniel Rodríguez Pivaral <149130487+daniel102sa1@users.noreply.github.com>
2026-08-26 18:24:29 -0600

    feat: add Binance positions page

---
+++ app/positions/page.tsx
@@ -0,0 +1,7 @@
+import BinanceLivePositionCoach from "@/components/BinanceLivePositionCoach";
+
+export const dynamic = "force-dynamic";
+
+export default function PositionsPage() {
+  return <BinanceLivePositionCoach />;
+}
